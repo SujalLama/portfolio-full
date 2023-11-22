9443c7c03f4f7ebfb2802457c0b4afee801a9230
sujal lama <lsujal41@gmail.com>
2023-11-22 16:10:57 +0545

    project path changed

--- frontend/src/api/constants.ts
+++ frontend/src/api/constants.ts
@@ -9,7 +9,7 @@ export const ALL_BLOGS_PATH = API_URL + '/blogs?fields[1]=title&fields[2]=descri
 export const RELATED_BLOGS_PATH = API_URL + '/blogs?fields[1]=title&fields[2]=description&fields[3]=slug&populate[0]=cover&populate[1]=tags&pagination[pageSize]=4';
 
 
-export const PROJECTS_PATH = API_URL + '/projects?fields[1]=title&fields[2]=description&fields[3]=slug&populate[0]=banner&populate[1]=tags&pagination[pageSize]=6';
+export const PROJECTS_PATH = API_URL + '/projects?fields[1]=title&fields[2]=description&fields[3]=slug&populate[0]=banner&populate[1]=tags&pagination[pageSize]=6&sort[0]=updatedAt:desc';
 export const ALL_PROJECTS_PATH = API_URL + '/projects?fields[1]=title&fields[2]=description&fields[3]=slug&populate[0]=banner&populate[1]=tags';
 
 export const PROJECTS_SLUG_PATH = API_URL + '/projects?fields[0]=slug'
